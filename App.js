c02df536cfdfc64f6f647cb682ab5073c852b275
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-05-06 22:08:40 +0300

    simple-app

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  //state hook
+  //second functions helps you se state
+  const [outputText, setOutputText] = useState(
+    "Open up App.js to start working on your app!"
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button
+        title='change text'
+        onPress={() => setOutputText("The text changed")}
+      />
     </View>
   );
 }
@@ -12,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
